d7c739c2d066a5d73199ae39015d397a56822ee7
colmisch123 <colemische@gmail.com>
2025-10-21 22:50:36 -0500

    Comment updates

--- Final project table drops.sql
+++ Final project table drops.sql
@@ -1,7 +1,3 @@
---Cole Mische
---Misch123
---CS4322 Summer 2025 Final project
-
 --This block wipes all tables and resets the ID counter. Use with caution!
 DO $$
 BEGIN
@@ -19,4 +15,5 @@ BEGIN
     ALTER SEQUENCE global_id_seq RESTART;
 END;
 $$ LANGUAGE plpgsql;
+
 
